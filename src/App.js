791c0c68b3051c3615e7e4bf06e22d108190c720
Ali Ahmad <turgut@Alis-MacBook-Pro.local>
2022-01-16 18:00:51 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Book Store</h1>
+      <p>This is the best book store ever</p>
+      <img alt="Book Store Image" src="https://s2982.pcdn.co/wp-content/uploads/2020/04/bookstore-books-feature-700x375-1.jpg.webp" />
+    
     </div>
   );
 }
